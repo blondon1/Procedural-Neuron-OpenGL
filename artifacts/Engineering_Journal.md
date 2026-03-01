f131e2ec5eed59ff1f23275219658f39057c892b
Bryan <bryanlondres5@gmail.com>
2026-03-01 18:40:51 -0500

    Refactor rendering pipeline to use procedural geometry and OOD Neuron class

--- artifacts/Engineering_Journal.md
+++ artifacts/Engineering_Journal.md
@@ -11,7 +11,7 @@ Link: *https://www.nature.com/articles/s41598-021-81421-2*
 
 Neural models are quite varied in complexity and in how many factors they consider, I think I should use the Leaky Integrate and Fire(LIF) model because of its simplicity, it is still a highly efficient linear encoder.
 Lecture from Dr. Mario Negrello from the Erasmus Medical Center, Netherlands
-https: *//www.youtube.com/watch?v=xx45K9fuuO4&t=47s*
+Link: *//www.youtube.com/watch?v=xx45K9fuuO4&t=47s*
 
 # 2/27/2026
 I need to understand my environment deeply before I start engineering solutions for the problems of showcasing two neurons talking to each other through electrical signals first in 2D and then in 3D.
@@ -30,3 +30,36 @@ I need to understand my environment deeply before I start engineering solutions 
 
 ## Biological Anatomy
 The soma(body of the neuron) has the ability to send electrical signals through the axon and receive electrical signals from the dendrites to communicate with other neurons, I need to simulate first the body of the cell by iterating over the area of the circle we can get the sine and cosine for each angle and multiply them by the radius to get a circle.
+
+![e.g.](Complete_neuron_cell_diagram.png)
+
+# 2/28/2026
+OpenGL has been a learning experience in udnerstanding the pipeline between CPU logic and raw GPU execution. At first, I was treating the graphics pipeline like a standard script—trying to force GLSL directly into my C++ environment and misunderstanding how the state machine tracks memory. I had to tear that down and rebuild my mental model around the strict Core Profile architecture: using VBOs to essentially rent out raw VRAM for my coordinate payloads, and VAOs as the orchestrators that tell the graphics card exactly how to parse that memory during the render loop. The real turning point was moving past hardcoded, static float arrays. Figuring out why my four vertices initially rendered a right triangle instead of a full rectangle forced me to understand how OpenGL fundamentally rasterizes primitives. Now, instead of manually plotting points, I'm shifting to procedural generation—encapsulating the math, into object-oriented structures that dynamically calculate and push coordinates into vector buffers. 
+
+## Brainstorming on engineering dilemmas for the graphics programming simulation
+
+Need a circle and an oval so the GL_TRIANGLE_FAN primitive should work for the soma and nuclei, using a central point and triangles from it and using the sin and cosine to calculate the radius, mark points on the circumference and connect them, when there's a big enough number of them the regular polygon will have so many straight edges that it looks like a smooth circle to the human eye
+
+L-Systems algorithms are used to simulate the growth of fractal topologies like plants so I think I could apply this to the dendrites.  
+
+![e.g.](LSystem.png)
+
+I should use an an **Orthographic Projection Matrix construct** to allow definitions of C++
+coordinates using biological units like micrometers, μm
+
+![e.g.](ortho-proj3.png)
+
+**Subdivided Geometry** vs **Shader UV Propagation** for the LIF modeling of exponential charge in the neurons, **Subdivided Geometry** may cause the dentrite to have too many segments and LIF equations so it will scale terribly so **Shader UV Propagation** should allow me to unify the entire dentrite mathematically(LIF equation per neuron)
+
+**Notes:**
+The soma is essentialy a biological capacitor filled with ions
+Key concepts of the LIF model: the neurons sums up electrical signals from other neurons, without input the voltage(electrical charge) slowly leaks away but if it crosses a threshold it creates a spike and after that it resets the voltage.
+
+![e.g.](Leaky_Integrate-and-Fire_model_neuron.jpg)
+
+![e.g.](neuron_anatomy.jpg)
+
+# 3/1/2026
+
+OOD(Object Oriented Design)
+So I have the following components that I have to implement in code, the nucleus as the DNA holder of the cell, the soma, the dendrites and the axon, firstly I want to focus on the nucleus and the soma to have a visual representation with a dark blue for the nucleus and a light blue for the soma, I will have a struct for all of them and a Neuron class to own the building parts of the cell, because there is a difference between has-a and is-a in OOD and a neuron owns its parts, but its parts do not inherit from them. Need a base class for the architecture to contain the VBO and VAO context to not have to repeat myself in the main function. The struct building components of the cell will inherit from the base class renderShape(not the Neuron class). The base class will 1- declare the VBO and VAO variables 2- generate the math on the cpu side for the shapes 3- generate the VBO object and VAO state manager on the GPU 4- draw the elements.
--- src/2D_topology.cpp
+++ src/2D_topology.cpp
@@ -1,34 +1,209 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 #include <iostream>
+#include <glm/glm.hpp>
 #include <vector>
 #include <cmath>
+#include <numbers>
+#define _USE_MATH_DEFINES
+
+// Define Shader Source Code (GLSL - C like)
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "uniform vec4 objectColor;\n" 
+    "void main()\n"
+    "{\n"
+    "   FragColor = objectColor;\n" 
+    "}\n";
 
 
+// Owns the GPU memory and the geometric payload
+struct RenderableShape {
+    // Allocate Memory (Define Vertices) & Record the State
+    unsigned int VAO = 0, VBO = 0;
+    std::vector<glm::vec3> vertices;
+    float M_PI = std::numbers::pi_v<float>;
+    // Mathematical generator on the CPU side
+    // Generates a perfect circle if radiusX == radiusY, or an oval if they differ.
+    void generateCurvilinearShape(float radiusX, float radiusY, int segments) {
+        vertices.clear();
+        
+        // Origin Point for the GL_TRIANGLE_FAN
+        vertices.push_back(glm::vec3(0.0f, 0.0f, 0.0f)); 
+
+        // Circunference points
+        float angleStep = (2.0f * M_PI) / segments;
+        for (int i = 0; i <= segments; i++) {
+            float currentAngle = i * angleStep;
+            float x = radiusX * cos(currentAngle);
+            float y = radiusY * sin(currentAngle);
+            vertices.push_back(glm::vec3(x, y, 0.0f));
+        }
+    }
+    /* memory allocator on the GPU Side which needs the OpenGL context (GLFW window)
+    to avoid a fatal crash */
+    void allocateVram() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        // Extracting raw bytes and pointers from the C++ std::vector
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    // Execution
+    void Draw(GLenum drawingMode, unsigned int shaderProgram, float r, float g, float b, float a) {
+        int location = glGetUniformLocation(shaderProgram, "objectColor");
+        glUniform4f(location, r, g, b, a);
+        glBindVertexArray(VAO);
+        glDrawArrays(drawingMode, 0, vertices.size());
+        glBindVertexArray(0); // Unbind to protect state
+    }
+
+    // Destructor to deallocate memory
+        ~RenderableShape() {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+    }
+
+};
+
+// The Derived Classes: They inherit VAO, VBO, and vertices 
+struct Nucleus : public RenderableShape {
+    Nucleus() {
+        float radiusX = 0.1f; // narrower width
+        float radiusY = 0.15f; // taller height for an oval shape
+        int visualFidelity = 36;
+        generateCurvilinearShape(radiusX, radiusY, visualFidelity);
+    }
+};
+
+struct Soma : public RenderableShape {
+    // Constructor to define the shape
+    Soma() {
+        float radius = 0.3f; // equal radius for a circle
+        int visualFidelity = 36; // 36 segments for a smooth circle
+        generateCurvilinearShape(radius, radius, visualFidelity);
+    }
+};
+
+// TODO
+struct Dendrite : public RenderableShape {};
+struct Axon : public RenderableShape {};
+
+// The Manager Class which owns the biology and the state
+struct Neuron {
+    glm::vec2 position; 
+
+    // LIF Model State Variables (For later use)
+    float membrane_potential = -65.0f;
+    float resting_potential = -65.0f;
+    float threshold = -50.0f;
+
+    // Composition: The Neuron HAS these structural parts
+    Soma soma;
+    Nucleus nucleus;
+    Dendrite dendrite;
+    Axon axon;
+
+    // Called once from main() after window creation
+    void initializeHardware() {
+        soma.allocateVram();
+        nucleus.allocateVram();
+        // axon.allocateVram(); 
+        // dendrite.allocateVram();
+    }
+
+    // Called every frame inside the render loop
+    void Draw(unsigned int shaderProgram) {
+        // Draw Soma: Dark Blue (R: 0.1, G: 0.2, B: 0.5, A: 1.0)
+        soma.Draw(GL_TRIANGLE_FAN, shaderProgram, 0.0f, 0.651f, 1.0f, 1.0f);
+        
+        // Draw Nucleus: Light Grey/Yellow (R: 0.8, G: 0.8, B: 0.7, A: 1.0)
+        nucleus.Draw(GL_TRIANGLE_FAN, shaderProgram, 0.0f, 0.0f, 0.38f, 1.0f);
+    }
+};
 
 int main() {
-    //GLFW
+    
+    
+
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Neuron Simulation", NULL, NULL);
-    if (!window) { glfwTerminate(); return -1; }
+    if (!window) { 
+        glfwTerminate(); 
+        return -1; 
+    }
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
 
 
-    // Rendering Loop
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    // Build the Pipeline (Compile & Link Shaders)
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //Scope for objects
+    {
+        // CPU side
+        Neuron myNeuron;
+        //GPU side
+        myNeuron.initializeHardware();
+
+        // Execution (The Render Loop)
+        while (!glfwWindowShouldClose(window)) {
+            /*graphics debugging
+            glUseProgram(shaderProgram);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            */
+
+            glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glUseProgram(shaderProgram);
+            // The entire biological drawing process is triggered by one command
+            myNeuron.Draw(shaderProgram);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
     }
+    
+
+    // Resource Deallocation
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
